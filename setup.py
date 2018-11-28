58e7a53815482b7778481f81cde95f53a60bb6f6
Dan Smith <dsmith@danplanet.com>
2018-11-28 12:20:04 -0800

    Call this 0.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='uvcclient',
-      version='0.10.1',
+      version='0.11.0',
       description='A remote control client for Ubiquiti\'s UVC NVR',
       author='Dan Smith',
       author_email='dsmith+uvcclient@danplanet.com',
